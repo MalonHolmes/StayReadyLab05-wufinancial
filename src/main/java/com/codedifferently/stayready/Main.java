1ab693e09ee79b9fbc715269a383549880803ae8
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-08 21:23:29 -0400

    Skeletons made...gonna complete tm

--- src/main/java/com/codedifferently/stayready/Main.java
+++ src/main/java/com/codedifferently/stayready/Main.java
@@ -1,4 +1,46 @@
 package com.codedifferently.stayready;
 
 public class Main {
+
+    public static Double fromUSToEuro(Double USDollars){
+        return null;
+    }
+
+    public static Double fromEuroToUS(Double euros){
+        return null;
+    }
+
+    public static Double fromEuroToPound(Double euros){
+        return null;
+    }
+
+    public static Double fromPoundToRupee(Double pounds){
+        return null;
+    }
+
+    public static Double fromRupeeToCanadianDollar(Double rupee){
+        return null;
+    }
+
+    public static Double fromCanadianDollarToSingaporeDollar(Double canDollars){
+        return null;
+    }
+
+    public static Double fromSingaporeDollarToFranc(Double singDollars){
+        return null;
+    }
+
+    public static Double fromFrancToRinggit(Double francs){
+        return null;
+    }
+
+    public static Double fromRinggitToYen(Double ringgits){
+        return null;
+    }
+
+    public static Double fromYenToRenminbi(Double yens){
+        return null;
+    }
+
+
 }
